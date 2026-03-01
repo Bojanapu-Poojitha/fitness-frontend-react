f109fb8cd0a28b1b7393e6c0cec1c7b9bb14986d
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-01 19:07:10 +0530

    Add a form submit function and if condition

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -6,4 +6,12 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [image, setImage] = useState<File | null>(null);
   const [message, setMessage] = useState("");
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+  if (!image) {
+    setMessage("select a profile image is mandatory");
+    return;
+  }
 };
