5d34e72e98fea03159fed9846ce0f8dbffef0a98
Bojanapu-Poojitha <poojitha.b@everest.engineering>
2026-03-01 19:03:32 +0530

    Define a required fields for signup page using usestate

--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,3 +1,9 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 
-
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [image, setImage] = useState<File | null>(null);
+  const [message, setMessage] = useState("");
+};
